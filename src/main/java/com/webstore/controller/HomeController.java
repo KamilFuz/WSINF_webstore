2278acfcaf99fc3a59c17c9582d1dc86be01863f
Kamil Fuz <fuzkamil1993@gmail.com>
2018-07-21 23:03:22 +0200

    Adding New User Controller

--- src/main/java/com/webstore/controller/HomeController.java
+++ src/main/java/com/webstore/controller/HomeController.java
@@ -2,8 +2,11 @@ package com.webstore.controller;
 
 import com.webstore.domain.User;
 import com.webstore.domain.security.PasswordResetToken;
+import com.webstore.domain.security.Role;
+import com.webstore.domain.security.UserRole;
 import com.webstore.service.impl.UserSecurityService;
 import com.webstore.service.impl.UserService;
+import com.webstore.utility.SecurityUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -11,10 +14,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 @Controller
 public class HomeController {
@@ -49,6 +57,50 @@ public class HomeController {
 //        PasswordResetToken passToken = userService.getPasswordResetToken(token);
         model.addAttribute("classActiveForgetPassword", true);
         return "myAccount";
+    }
+
+    @RequestMapping(value="/newUser", method=RequestMethod.POST)
+    public String newUserPost(
+            HttpServletRequest request,
+            @ModelAttribute("email") String userEmail,
+            @ModelAttribute("username") String username,
+            Model model
+    ) throws Exception{
+        model.addAttribute("classAcitveNewAccount", true);
+        model.addAttribute("email", userEmail);
+        model.addAttribute("username", username);
+
+        if (userService.findByUsername(username) != null) {
+            model.addAttribute("usernameExists", true);
+
+            return "myAccount";
+        }
+
+        if (userService.findByEmail(userEmail) != null) {
+            model.addAttribute("email", true);
+
+            return "myAccount";
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(userEmail);
+
+        String password = SecurityUtility.randomPassword();
+
+        String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
+        user.setPassword(encryptedPassword);
+
+        Role role = new Role();
+        role.setRoleId(1);
+        role.setName("ROLE_USER");
+        Set<UserRole> userRoles = new HashSet<>();
+        userRoles.add(new UserRole(user, role));
+        userService.createUser(user, userRoles);
+
+    }
+    ) {
+
     }
 
     @RequestMapping("/newUser")
--- src/main/java/com/webstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/webstore/service/impl/UserServiceImpl.java
@@ -3,11 +3,15 @@ package com.webstore.service.impl;
 import com.webstore.domain.User;
 import com.webstore.domain.security.PasswordResetToken;
 import com.webstore.repository.PasswordResetTokenRepository;
+import com.webstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements com.webstore.service.impl.UserService {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private PasswordResetTokenRepository passwordResetTokenRepository;
@@ -21,5 +25,14 @@ public class UserServiceImpl implements com.webstore.service.impl.UserService {
     public void createPasswordResetTokenForUser(final User user,final String token) {
         final PasswordResetToken myToken = new PasswordResetToken(token, user);
         passwordResetTokenRepository.save(myToken);
+    }
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
