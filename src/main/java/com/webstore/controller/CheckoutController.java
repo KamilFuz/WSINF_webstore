4ba72b23dd5d9e6dcb1cac58e93ee481d83670fd
Kamil Fuz <fuzkamil1993@gmail.com>
2018-10-06 21:45:01 +0200

    Handling order submit

--- src/main/java/com/webstore/controller/CheckoutController.java
+++ src/main/java/com/webstore/controller/CheckoutController.java
@@ -5,14 +5,19 @@ import com.webstore.service.*;
 import com.webstore.service.impl.UserService;
 import com.webstore.utility.USConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 public class CheckoutController {
@@ -20,6 +25,12 @@ public class CheckoutController {
     private ShippingAddress shippingAddress = new ShippingAddress();
     private BillingAddress billingAddress = new BillingAddress();
     private Payment payment = new Payment();
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
 
     @Autowired
     private UserService userService;
@@ -118,6 +129,58 @@ public class CheckoutController {
         }
 
         return "checkout";
+    }
+
+    @RequestMapping(value = "/checkout", method = RequestMethod.POST)
+    public String checkoutPost(@ModelAttribute("shippingAddress") ShippingAddress shippingAddress,
+                               @ModelAttribute("billingAddress") BillingAddress billingAddress, @ModelAttribute("payment") Payment payment,
+                               @ModelAttribute("billingSameAsShipping") String billingSameAsShipping,
+                               @ModelAttribute("shippingMethod") String shippingMethod, Principal principal, Model model) {
+        ShoppingCart shoppingCart = userService.findByUsername(principal.getName()).getShoppingCart();
+
+        List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
+        model.addAttribute("cartItemList", cartItemList);
+
+        if (billingSameAsShipping.equals("true")) {
+            billingAddress.setBillingAddressName(shippingAddress.getShippingAddressName());
+            billingAddress.setBillingAddressStreet1(shippingAddress.getShippingAddressStreet1());
+            billingAddress.setBillingAddressStreet2(shippingAddress.getShippingAddressStreet2());
+            billingAddress.setBillingAddressCity(shippingAddress.getShippingAddressCity());
+            billingAddress.setBillingAddressState(shippingAddress.getShippingAddressState());
+            billingAddress.setBillingAddressCountry(shippingAddress.getShippingAddressCountry());
+            billingAddress.setBillingAddressZipcode(shippingAddress.getShippingAddressZipcode());
+        }
+
+        if (shippingAddress.getShippingAddressStreet1().isEmpty() || shippingAddress.getShippingAddressCity().isEmpty()
+                || shippingAddress.getShippingAddressState().isEmpty()
+                || shippingAddress.getShippingAddressName().isEmpty()
+                || shippingAddress.getShippingAddressZipcode().isEmpty() || payment.getCardNumber().isEmpty()
+                || payment.getCvc() == 0 || billingAddress.getBillingAddressStreet1().isEmpty()
+                || billingAddress.getBillingAddressCity().isEmpty() || billingAddress.getBillingAddressState().isEmpty()
+                || billingAddress.getBillingAddressName().isEmpty()
+                || billingAddress.getBillingAddressZipcode().isEmpty())
+            return "redirect:/checkout?id=" + shoppingCart.getId() + "&missingRequiredField=true";
+
+        User user = userService.findByUsername(principal.getName());
+
+        Order order = orderService.createOrder(shoppingCart, shippingAddress, billingAddress, payment, shippingMethod, user);
+
+        mailSender.send(mailConstructor.constructOrderConfirmationEmail(user, order, Locale.ENGLISH));
+
+        shoppingCartService.clearShoppingCart(shoppingCart);
+
+        LocalDate today = LocalDate.now();
+        LocalDate estimatedDeliveryDate;
+
+        if (shippingMethod.equals("groundShipping")) {
+            estimatedDeliveryDate = today.plusDays(5);
+        } else {
+            estimatedDeliveryDate = today.plusDays(3);
+        }
+
+        model.addAttribute("estimatedDeliveryDate", estimatedDeliveryDate);
+
+        return "orderSubmittedPage";
     }
 
     @RequestMapping("/setShippingAddress")
